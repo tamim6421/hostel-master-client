ed3c0dba09b2e937f6429ff1556d1d3989ffd724
ProgrammingHero <tamimhossain6421@gmail.com>
2023-12-09 19:16:12 +0600

    show area

--- src/components/Rooms/Area/Area.jsx
+++ src/components/Rooms/Area/Area.jsx
@@ -5,11 +5,14 @@ import { AreaIcons } from "./AreaData";
 
 const Area = () => {
     return (
-        <div>
            <Container>
+            <div>
+                <h1 className="mt-20 mb-10 text-center  text-3xl font-bold">Select Your Area </h1>
+            </div>
+        <div className="pt-4 flex gap-3 overflow-hidden flex-wrap items-center content-center justify-center ">
            {AreaIcons?.map( area => <AreaBox key={area.label} label={area.label} icon={area.icon}></AreaBox>)}
-           </Container>
         </div>
+           </Container>
     );
 };
 
--- src/components/Rooms/Area/AreaBox.jsx
+++ src/components/Rooms/Area/AreaBox.jsx
@@ -1,10 +1,19 @@
+import { useSearchParams } from "react-router-dom";
 
 
 const AreaBox = ({label, icon: Icon}) => {
-    console.log(label)
+    const [params , setParams] = useSearchParams()
+    
+    // console.log(label)
     return (
-        <div>
-            <p>{label}</p>
+        <div className="flex box flex-col  rounded-lg  items-center justify-center p-3 gap-2 border-b-2  hover:text-gray-900 transition cursor-pointer ">
+       
+                <div>
+              
+
+                </div>
+           
+            <div className="text-lg font-bold ">{label}</div>
         </div>
     );
 };
